0e1b50a72695bc2939ed1080f81ed28aac5c9661
Yann Cabrel NGATEU TAGNE <ngateuyann8@gmail.com>
2026-02-12 09:22:46 +0100

    initial commit

---
+++ src/test/java/com/yann/smart_valuator_api/SmartValuatorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yann.smart_valuator_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartValuatorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
